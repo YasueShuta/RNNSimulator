ccb1ca0bef6d5a68d99b12f0dae1d39966f15124
Shuta Yasue <yasue.brain59074@gmail.com>
2016-01-07 13:57:46 +0900

    add SimTime

--- class/Simulator/SimTime.h
+++ class/Simulator/SimTime.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "../Abstract/Findable.h"
+#include <vector>
 
 namespace RNNSimulator {
+	class SimTime : public Findable {
+	public:
+		std::vector<double> data;
+		int ti;
+		double now;
+		int len;
+		double nsecs;
 
+		double dt;
+
+		SimTime(double nsecs = 144.0, double dt = 0.1);
+		~SimTime();
+	};
 }
